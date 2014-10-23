336aef61083096f0399fdbb771e75b88d5ae7dd1
Julian Einwag <julian.einwag@marcapo.com>
2014-10-23 10:30:31 +0200

    Fix.

--- config/software/server-jre.rb
+++ config/software/server-jre.rb
@@ -29,7 +29,7 @@ if Ohai["kernel"]["machine"] =~ /x86_64/
   source url:     "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/server-jre-7u25-linux-x64.tar.gz",
          md5:     "7164bd8619d731a2e8c01d0c60110f80",
          cookie:  "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie",
-         warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html",
+         warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
 else
   raise "Server-jre can only be installed on x86_64 systems."
 end
