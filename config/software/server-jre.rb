1596b1d84231f0186304ab60d76aed44a95accba
Julian Einwag <julian.einwag@marcapo.com>
2014-10-23 10:27:40 +0200

    Removed unsafe flag.

--- config/software/server-jre.rb
+++ config/software/server-jre.rb
@@ -30,7 +30,6 @@ if Ohai["kernel"]["machine"] =~ /x86_64/
          md5:     "7164bd8619d731a2e8c01d0c60110f80",
          cookie:  "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie",
          warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html",
-         unsafe:  true
 else
   raise "Server-jre can only be installed on x86_64 systems."
 end
